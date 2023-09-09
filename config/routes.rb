7aac73decc1f302c84b8948ffd31d424f4f07ec3
David Katende <katendedavidjoga@gmail.com>
2023-09-09 22:42:09 +0300

    Setup Rswag

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', confirmations: 'users/confirmations' }
 
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
